b1d9241d9e78111210c939334aeaa64a8d2307e8
Egawa Takashi <egawa.takashi@gmail.com>
2024-05-09 23:32:16 +0900

    display read image

--- ui/src/renderer.ts
+++ ui/src/renderer.ts
@@ -1,10 +1,1 @@
-const btn = document.getElementById('read-file');
-const filePathElement = document.getElementById('filePath');
-
-console.log('--------------------------------------------------------')
-
-//
-btn?.addEventListener('click', async () => {
-    const filePath = await (window as any).electronAPI.openFile()
-    filePathElement!.innerText = filePath
-})
+// no content
--- ui/src/web/App.tsx
+++ ui/src/web/App.tsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import { Helmet } from 'react-helmet';
 
 // aa
 export const App = () => {
+  var [inputImage, setInputImage] = useState('')
+
+  const handleReadFile = async () => {
+      const filePath = await (window as any).electronAPI.openFile()
+      setInputImage(filePath)
+  }
+
   return (
     <div className="container">
-        <InputImage src="./images/input.jpg" />
+        <InputImage src={inputImage} />
         <OutputImage src="./images/output.jpg" />
         <input type="text" name="threshold1" value="100" />
         <input type="text" name="threshold2" value="200" />
-        <button type="button" id="read-file">Read File</button>
+        <button type="button" onClick={handleReadFile}>Read File</button>
         file path: <strong id="filePath"></strong>
         <button>Generate</button>
         <button>Export</button>
@@ -26,7 +34,7 @@ interface InputImageProps {
 
 const InputImage: React.FC<InputImageProps> = ({ src }) => {
   return (
-    <img src={src} width="300" alt="input" />
+    src === '' ? <div>input image</div> : <img src={src} width="300" alt="input" />
   );
 };
 
@@ -36,6 +44,6 @@ interface outputImageProps {
 
 const OutputImage: React.FC<outputImageProps> = ({ src }) => {
   return (
-    <img src={src} width="300" alt="output" />
+    src === '' ? <div>output image</div> : <img src={src} width="300" alt="output" />
   );
 };
